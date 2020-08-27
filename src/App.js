598a0ec46ab2602d84b90eb377ca92cb87ae204f
glassbones <glassbonesofficial@gmail.com>
2020-08-27 13:09:53 -0500

    bout to try something pretty extra

--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import produce from 'immer';
+import useWindowDimensions from './utils/useWindow';
 
 //init vars
-const colNum = 50
-const rowNum = 50
-const cellSize = 15
-
+//const colNum = 50
+//const rowNum = 32
+const cellSize = 30
+const updateSpeed = 500
+// [0,1] [0,-1] [1,-1] [-1,1] [1,1] [-1,-1] [1,0],[-1,0]
+const ops = Array(9).fill(1).map( (i,num) => [Math.floor(num/3) - 1 ,num % 3 - 1] )
 const gridStyle = () => ({
     display: 'grid',
     gridTemplateColumns: `repeat(${rowNum}, ${cellSize}px)`,
   }
 )
-
-//height: rowNum*cellSize,
-//width: '100%',
-
-// cell styling
 const cellStyle = (bool) => ({
     width: cellSize,
     height: cellSize,
-    backgroundColor: bool ? "pink" : undefined,
-    border: "solid 1px grey"
+    backgroundColor: bool ? undefined : "black",
+    border: "solid 1px black"
   }
 )
 
-// [0,1] [0,-1] [1,-1] [-1,1] [1,1] [-1,-1] [1,0],[-1,0]
-const ops = Array(9).fill(1).map( (i,num) => [Math.floor(num/3) - 1 ,num % 3 - 1] )
 
+// App
 const App = () => {
+  const { height, width } = useWindowDimensions();
+  const [colNum, setColNum] = useState(50)
+  const [rowNum, setRowNum] = useState(10)
+
   //create state for grid. (colNum x rowNum)
   const [grid, setGrid] = useState(Array.from({length: colNum}).map(() => Array.from({length: rowNum}).fill(0)))
+  //create state for start/stop button 
   const [isActive, setIsActive] = useState(false)
+  // really ugly statemanagement to avoid loop during simulate()
+  const activeRef = useRef(isActive)
+  activeRef.current = isActive
 
-  const simulate = useCallback(()=>{
-    if (!isActive) return;
+  
 
-    setGrid((currentGrid) => {
-      return produce(currentGrid, gridClone =>{
+  // game logic loop
+  const simulate = useCallback(() => {
+    if (!activeRef.current) return;
+
+    setGrid((currentGrid) => (
+      produce(currentGrid, gridClone =>{
         //for each cell
         for(let row = 0; row < rowNum; row++){
           for(let col = 0; col < colNum; col++){
-            
             let neighbors = 0;
             // itterate through neighbors via the opps array
             ops.forEach(([x,y]) => {
@@ -52,55 +59,55 @@ const App = () => {
                 neighbors += currentGrid[neighborX][neighborY]
               }
             })
-
             // if 1 or 4 neighbors cell dies next gen 💩
-            if (neighbors < 2 || neighbors > 3) gridCopy[row][col] = 0
+            if (neighbors < 2 || neighbors > 3) gridClone[row][col] = 0
             // if current cell is dead and has 3 neighbors it is born next gen 👍
             else if (currentGrid[row][col] === 0 && neighbors === 3) gridClone[row][col] = 1
-              
-            
           }
         }
       })
-    })
+    )) 
+    //delay before updating
+    setTimeout(simulate, updateSpeed); 
+  },[]);
 
 
-    //rate
-    setTimeout(simulate, 1000)
-  }, [isActive])
-  //useEffect(() => running && runSimulation(), [running])
-
+  
   return (
-    <>
-      <button onClick={()=> setIsActive(!isActive)}>
+    <div>
+      <div>
+        width: {width} ~ height: {height}
+      </div>
+      <div style={gridStyle()}>
+        {grid.map((row, rowIdx) =>
+          row.map((col, colIdx) => (
+            <div
+              key={`${rowIdx}-${colIdx}`}
+              onClick={()=> {
+                //onclick clone the grid -------> in clone set cell to !cell ------> returns as setState(clonedState) 🙂 THANKS IMMER!
+                setGrid(produce(grid, gridClone => {gridClone[rowIdx][colIdx] ^= 1}))
+              }}
+              // if (cell logic here to change background color if not)
+              style={cellStyle(grid[rowIdx][colIdx])}           
+            />
+          ))   
+        )}
+      </div>
+
+      <button //start button
+        onClick={()=> {
+          //ugly start/stop logic on click
+          setIsActive(!isActive)
+          if (!isActive){
+            activeRef.current = true
+            simulate()
+          }
+        }}>
         {isActive ? 'stop' : 'start' }
       </button>
 
-      <div style={gridStyle()}>
 
-          {grid.map((row, rowIdx) =>
-            row.map((col, colIdx) => (
-              <div
-                key={`${rowIdx}-${colIdx}`}
-                onClick={()=> {
-                  //onclick clone the grid -------> in clone set cell to !cell ------> returns as setState(clonedState) 🙂 THANKS IMMER!
-                  setGrid(produce(grid, gridCopy => {gridCopy[rowIdx][colIdx] ^= 1}))
-                }}
-                // if (cell logic here to change background color if not)
-                style={cellStyle(grid[rowIdx][colIdx])}           
-              />
-            ))   
-          )}
-        </div>
-
-
-    </>
+    </div>
   )
 }
-
 export default App;
-/*
-[...Array(5).keys()].forEach((i) => { 
-  rows.push(Array.from(Array(numCols), () => 0 ))
-})
-*/
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import ReactDOM from "react-dom";
+import React from "react";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    
+    <>
+        <CssBaseline />
+        <App />
+    </>, 
+
+document.getElementById("root"));
